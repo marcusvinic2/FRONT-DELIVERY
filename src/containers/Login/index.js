bf4507a540aef2fed4fed50ebe4569b475d07bb3
Marcus Vinicius <18752038+marcusvinic2@users.noreply.github.com>
2022-02-09 14:56:28 -0300

    estrutura

---
+++ src/containers/Login/index.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+function Login(){
+
+    return (
+    <div>
+        <h1>LOGIN</h1>
+    </div>
+    )
+}
+
+export default Login
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Login from './containers/Login'
 
 ReactDOM.render(
-    <App />,
+    <Login />,
   document.getElementById('root')
 );
